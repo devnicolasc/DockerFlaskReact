50017e0246776676f537b575287d23b366c733d7
devnicolasc <nicolascho2@gmail.com>
2021-05-20 16:34:02 +0300

    flask

--- server/app.py
+++ server/app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def index():
-    return('hello')
+    return {
+        "test": "result1",
+        "test2": "result2"
+    }
 
 
 if __name__ == '__main__':
