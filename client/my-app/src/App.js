7b20586718d66d296ddc3395f799e82b0e5ed7d1
devnicolasc <nicolascho2@gmail.com>
2021-06-24 21:03:05 +0300

    first commit

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,29 +1,6 @@
-import './App.css';
-import React, {useEffect, useState} from 'react';
-import { ReactTable } from "./Components/ReactTable";
-//? ---Effect Hook---, let you use state and other React features without writing a class 
-//? and lets you perform side effects in function components:.
-//?  ---State Hook---, https://reactjs.org/docs/hooks-state.html
-
 
 function App() {
-  // const [initialState, setState] = useState([])
-  const url =  '/api';
-  const [tabledata, setTabledata] = useState([])
-  
-  useEffect(()=> {
-    fetch(url).then(response => 
-      response.json().then(data => {
-        setTabledata(data.r)
-      })
-    );
-  }, []);
-
-  console.log(tabledata)
-
-  return <div className="App">
-           <ReactTable table_data={tabledata} />
-         </div>
+  return <div >Hello</div>
 
 }
 
